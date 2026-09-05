e73389518b24974bc8561fe95aff6a322154465d
MONSIEUR-RBG <eyramrizbangou@gmail.com>
2026-09-05 19:30:06 +0200

    Leap year finished...

---
+++ part01-Part01_36.LeapYear/src/main/java/LeapYear.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class LeapYear {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        
+        System.out.println("Give a year:");
+        int userInput = Integer.valueOf(scan.nextLine());
+        
+        if(userInput % 4 != 0){
+            System.out.println("The year is not a leap year.");
+            
+        } else if((userInput % 4 == 0) && (userInput % 100 == 0)){
+            if(userInput % 400 == 0){
+                System.out.println("The year is a leap year.");
+            }else {
+                System.out.println("The year is not a leap year.");
+            }
+        }else{
+            System.out.println("The year is a leap year.");
+        }
+
+    }
+}
